b1bc5c2c32ca15e2e6da04c6e5894029acbbd2ec
hjko <rhghwls0716@hanmail.net>
2022-07-19 14:19:27 +0900

    main

---
+++ 17. ATSG.tf
@@ -0,0 +1,17 @@
+resource "aws_placement_group" "hjko_pg" {
+  name = "hjko-pg"
+  strategy = "cluster"
+  }
+
+resource "aws_autoscaling_group" "hjko_atsg" {
+  name = "hjko-atsg"
+  min_size = 1
+  max_size = 10
+  health_check_grace_period = 60
+  health_check_type = "EC2"
+  desired_capacity = 1
+  force_delete = false
+  launch_configuration = aws_launch_configuration.hjko_lacf.name
+  vpc_zone_identifier = [aws_subnet.web_a.id, aws_subnet.web_c.id]
+
+}
